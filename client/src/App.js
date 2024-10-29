7656fa92fced03c6818544fe6f4961c95c4b7408
Tamas <tamas.fehertoi@gmail.com>
2024-10-29 13:44:32 -0500

    header content

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 
 function App() {
   return (
-    <main><header>
-      <a href="" className="logo">My-blog</a>
-      <nav>
-        <a href="">Login</a>
-        <a href="">Register</a>
-      </nav>
-    </header></main>
+    <main>
+      <header>
+        <a href="" className="logo">My-blog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="entry">
+        <img src="" alt="" />
+        <h2></h2>
+      </div>
+    </main>
   );
 }
 
