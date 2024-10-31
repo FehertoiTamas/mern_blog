225a00ef303adbb72afd1ab7236a683820dbea30
Tamas <tamas.fehertoi@gmail.com>
2024-10-31 11:24:02 -0500

    add content App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,47 @@ function App() {
           <a href="">Register</a>
         </nav>
       </header>
-      <div className="entry">
-        <img src="" alt="" />
-        <h2></h2>
+      <div className="post">
+        <div className="image">
+          <img src="https://blogs.incarail.com/hubfs/laguna-humantay-como-llegar-altitud-y-curiosidades-large-gps55i05c7.jpeg" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Humantay Lake túraélmény - Kék kristályvíz és perui hegyek csodája</h2>
+          <p className="info">
+            <a href="" className="author">Thomas</a>
+            <time datetime="">2024-10-31 11:14</time>
+          </p>
+          <p className="summary">Ha Peru gyönyörű tájait szeretnéd felfedezni, a Humantay-tó mindenképp legyen a bakancslistádon! Ez a kristálytiszta kék tó egy rejtett kincs a Salkantay-hegy közelében, és fantasztikus látványával mindenkit elvarázsol. Az utazás Cuscóból indul, és a túra maga közepesen nehéz – de a fáradtságért bőségesen kárpótol a panoráma!
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://blogs.incarail.com/hubfs/laguna-humantay-como-llegar-altitud-y-curiosidades-large-gps55i05c7.jpeg" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Humantay Lake túraélmény - Kék kristályvíz és perui hegyek csodája</h2>
+          <p className="info">
+            <a href="" className="author">Thomas</a>
+            <time datetime="">2024-10-31 11:14</time>
+          </p>
+          <p className="summary">Ha Peru gyönyörű tájait szeretnéd felfedezni, a Humantay-tó mindenképp legyen a bakancslistádon! Ez a kristálytiszta kék tó egy rejtett kincs a Salkantay-hegy közelében, és fantasztikus látványával mindenkit elvarázsol. Az utazás Cuscóból indul, és a túra maga közepesen nehéz – de a fáradtságért bőségesen kárpótol a panoráma!
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://blogs.incarail.com/hubfs/laguna-humantay-como-llegar-altitud-y-curiosidades-large-gps55i05c7.jpeg" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Humantay Lake túraélmény - Kék kristályvíz és perui hegyek csodája</h2>
+          <p className="info">
+            <a href="" className="author">Thomas</a>
+            <time datetime="">2024-10-31 11:14</time>
+          </p>
+          <p className="summary">Ha Peru gyönyörű tájait szeretnéd felfedezni, a Humantay-tó mindenképp legyen a bakancslistádon! Ez a kristálytiszta kék tó egy rejtett kincs a Salkantay-hegy közelében, és fantasztikus látványával mindenkit elvarázsol. Az utazás Cuscóból indul, és a túra maga közepesen nehéz – de a fáradtságért bőségesen kárpótol a panoráma!
+          </p>
+        </div>
       </div>
     </main>
   );
