fd91021a9250ae3df6ddeeac425ec445ab82c713
Tamas <tamas.fehertoi@gmail.com>
2024-10-31 14:43:38 -0500

    install bcrypt

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User')
 const app = express();
+const bcrypt = require('bcryptjs');
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -12,7 +15,10 @@ mongoose.connect("mongodb+srv://blog:aoY64cVWGkX6aOuv@cluster0.dwvfo.mongodb.net
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
   try {
-    const userDoc = await User.create({ username, password })
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    })
     res.json(userDoc);
   } catch (error) {
     res.status(400).json(error)
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 const RegisterPage = () => {
+
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+
   async function register(e) {
     e.preventDefault();
     const response = await fetch('http://localhost:4000/register', {
@@ -15,24 +17,24 @@ const RegisterPage = () => {
     } else {
       alert('registration failed');
     }
-
-    return (
-      <form className='register' onSubmit={register}>
-        <h1>Register</h1>
-        <input
-          type="text"
-          placeholder='username'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)} />
-        <input
-          type="password"
-          placeholder='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)} />
-        <button>Register</button>
-      </form>
-
-    )
   }
 
-  export default RegisterPage
+  return (
+    <form className='register' onSubmit={register}>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder='username'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)} />
+      <input
+        type="password"
+        placeholder='password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)} />
+      <button>Register</button>
+    </form>
+  )
+}
+
+export default RegisterPage
