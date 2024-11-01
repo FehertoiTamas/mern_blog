08d44943e2da13a1f0221e1dd8a30046be9b6186
Tamas <tamas.fehertoi@gmail.com>
2024-11-01 10:49:39 -0500

    add login methode

--- api/index.js
+++ api/index.js
@@ -4,10 +4,12 @@ const mongoose = require('mongoose');
 const User = require('./models/User')
 const app = express();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'asgfs565sdgt4sgfh4f63d3htthg'
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
 
 mongoose.connect("mongodb+srv://blog:aoY64cVWGkX6aOuv@cluster0.dwvfo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
@@ -22,6 +24,21 @@ app.post('/register', async (req, res) => {
     res.json(userDoc);
   } catch (error) {
     res.status(400).json(error)
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    // logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie('token', token).json('ok')
+    });
+  } else {
+    res.status(400).json('wrong credentials');
   }
 })
 
