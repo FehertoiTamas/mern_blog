0571191ec99a951fc596c06f08fb4ad67aa79cbc
Tamas <tamas.fehertoi@gmail.com>
2024-11-01 13:30:33 -0500

    add createPost page

---
+++ client/src/pages/CreatePost.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "+1" },
+      { indent: "-1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+];
+
+const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("")
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder={'Title'}
+        value={title}
+        onChange={(e) => { setTitle(e.target.value) }} />
+      <input
+        type="summary"
+        placeholder={'Summary'}
+        value={summary}
+        onChange={(e) => { setSummary(e.target.value) }} />
+      <input type="file" />
+      <ReactQuill
+        value={content}
+        onChange={newValue => { setContent(newValue) }}
+        modules={modules}
+        formats={formats} />
+      <button style={{ marginTop: '5px' }}>Create post</button>
+    </form >
+  )
+}
+
+export default CreatePost
