27ce8acf2d6a75ead17f4daaaed550c6e054b462
Tamas <tamas.fehertoi@gmail.com>
2024-11-02 14:34:34 -0500

    fetch post_id and add get point server side

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const PostPage = () => {
+  const [postInfo, setPostInfo] = useState(null);
+  const { id } = useParams();
+  useEffect(() => {
+    fetch(`http://localhost:4000/post/${id}`)
+      .then(response => {
+        response.json()
+          .then(postInfo => {
+            setPostInfo(postInfo);
+          })
+      });
+  }, []);
   return (
     <div>PostPage</div>
   )
