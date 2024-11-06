5992cbf0c3e3123a635fb7bfe86f2d5e0fcf3499
Tamas <tamas.fehertoi@gmail.com>
2024-11-06 18:29:13 -0500

    add edit post endpoint

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react'
+import { Navigate, useParams } from 'react-router-dom';
 import Editor from '../Editor';
 
 const EditPost = () => {
@@ -7,14 +7,43 @@ const EditPost = () => {
   const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
   const [files, setFiles] = useState("");
-  const [redirect, setRedirect] = useState("")
+  const [redirect, setRedirect] = useState(false);
+  const { id } = useParams();
 
-  function updatePost(e) {
+  useEffect(() => {
+    fetch(`http://localhost:4000/post/` + id)
+      .then(response => {
+        response.json().then(postInfo => {
+          setTitle(postInfo.title);
+          setContent(postInfo.content);
+          setSummary(postInfo.summary);
+        })
+      })
+  }, [])
+
+  async function updatePost(e) {
     e.preventDefault();
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    data.set('id', id);
+    if (files?.[0]) {
+      data.set('file', files?.[0]);
+    }
+
+    const response = await fetch('http://localhost:4000/post', {
+      method: 'PUT',
+      body: data,
+      credentials: 'include',
+    });
+    if (response.ok) {
+      setRedirect(true)
+    }
   }
 
   if (redirect) {
-    return <Navigate to={'/'} />
+    return <Navigate to={'/post/' + id} />
   }
 
   return (
@@ -31,7 +60,7 @@ const EditPost = () => {
         onChange={(e) => { setSummary(e.target.value) }} />
       <input type="file" onChange={(e) => setFiles(e.target.files)} />
       <Editor onChange={setContent} value={content} />
-      <button style={{ marginTop: '5px' }}>Create post</button>
+      <button style={{ marginTop: '5px' }}>Update post</button>
     </form >
   )
 }
